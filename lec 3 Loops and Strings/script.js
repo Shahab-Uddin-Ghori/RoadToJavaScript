ff0473b178669cf8e19b325bc6404a5e5b1ba12e
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-06-20 14:55:42 +0500

    for loop with example

---
+++ lec 3 Loops and Strings/script.js
@@ -0,0 +1,38 @@
+///////////////////////  loops ////////////////////////
+
+//Loops are computer programs that execute a set of instructions or a block of code a certain number of times without having to write it again until a certain condition is met. In other words, loops let your code execute one or more statements as many times as desired.
+
+/// for loop.
+console.log("\t\t\t\t counting number upto 10");
+for (let i = 0 ; i<=10; i++){
+    console.log(i);
+}
+// even number upto 40
+console.log("\t\t\t\teven number upto 40");
+for (let i = 0 ; i<=40 ; i+=2){
+    console.log(i);
+}
+// odd number upto 30
+console.log("\t\t\t\todd number upto 30");
+for (let i = 1 ;i<=30 ; i+=2){
+    console.log(i);
+}
+// using for loop sum of even numbes upto 20.
+console.log("\t\t\t\tsum of even numbers upto 20");
+let sum = 0;
+for (let i=2; i<=20; i+=2){
+    sum+=i
+}
+console.log("sum of even numbers upto 20 is \t" + sum);
+
+
+
+// Q1. Create a list of prime numbers using for loop upto 100.
+// prime numbers upto 40.
+// console.log("\t\t\t\tPrime number upto 30");
+// for (i=1; i<=50; i++){
+//     if (i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0){
+//         console.log(i);
+//     }
+// }
+
