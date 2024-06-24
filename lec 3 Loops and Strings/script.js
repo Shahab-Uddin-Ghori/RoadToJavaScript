039ce8b30d2b65ca101339f7309457924f1d7ba8
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-06-24 23:45:12 +0500

    nested loop example

--- lec 3 Loops and Strings/script.js
+++ lec 3 Loops and Strings/script.js
@@ -471,3 +471,23 @@ console.log(myArr[1][2]);
 console.log(myArr[2][0]);
 console.log(myArr[2][1]);
 console.log(myArr[2][2]);
+
+let arr = [1, 2, 3, 4, 5, 3, 4, 5];
+
+let a = 0;
+
+for (let i = 0; i < 3; i++) {
+  console.log("start");
+
+  for (let j = 0; j < 3; j++) {
+    console.log(`i is ${i}  j is ${j}`);
+    console.log(myArr[i][j]);
+    a++;
+  }
+  console.log("end");
+}
+
+// i = 0 0 0 1 1 1 2 2 2
+// j = 0 1 2 0 1 2 0 1 2
+
+console.log(a);
