c884797d8dca016c622dcf478265870589fa00c6
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-06-26 23:08:53 +0500

    shallow copy  Using Object.assign() method:

--- lec 4 arrays/script.js
+++ lec 4 arrays/script.js
@@ -501,3 +501,16 @@ console.log(originalArray); // [1, 2, 3, 4, 5] (original array is not affected)
 console.log(shallowCopy);   // [10, 2, 3, 4, 5]
 
 }
+
+{
+//Using Object.assign() method:
+
+  let originalArray = [1, 2, 3, 4, 5];
+let shallowCopy = Object.assign([], originalArray);
+
+// Modifying the shallow copy
+shallowCopy[0] = 10;
+console.log(originalArray); // [1, 2, 3, 4, 5] (original array is not affected)
+console.log(shallowCopy);   // [10, 2, 3, 4, 5]
+
+}
