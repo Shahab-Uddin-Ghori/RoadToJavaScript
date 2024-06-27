68937090d1a0f82167125e293ce139ceeefb06a7
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-06-27 19:07:51 +0500

    getAttribute

---
+++ 07 More on DOM/script.js
@@ -0,0 +1,14 @@
+// DOM Manipulation 
+// 1. Attributes..
+console.log("\t\t DOM Manipulation");
+console.log("\n\n\t Attribues");
+// getAttribute. bring the value of element attribures
+console.log("getAttribue");
+{
+    let firstDiv = document.querySelector("#firstDiv")
+    console.log(firstDiv);
+//    let firstDivAt = firstDiv.getAttribute("id")// id value // firstDiv
+    let firstDivAt = firstDiv.getAttribute("class") // class value //main
+   console.log(firstDivAt);
+}
+
