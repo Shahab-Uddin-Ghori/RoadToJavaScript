d83b5149efb2399c51add634f4ca5a57e462d7f6
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-06-27 19:15:28 +0500

    setAttribute

--- 07 More on DOM/script.js
+++ 07 More on DOM/script.js
@@ -12,3 +12,12 @@ console.log("getAttribue");
    console.log(firstDivAt);
 }
 
+// setAttribute. to set the new value of attrubute of element.
+let secDiv = document.querySelector("#secDiv")
+console.log(secDiv);
+console.log(secDiv.getAttribute("class"));// main
+// .. before class was main 
+secDiv.setAttribute("class","secMain")
+console.log(secDiv.getAttribute("class"));//secMain
+
+
